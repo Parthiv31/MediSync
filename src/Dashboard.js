54ae5fb16edd2d0a4ddaa387b1ea144e6bbedab9
Parthiv31 <parthivanil2002@gmail.com>
2023-11-12 02:46:59 +0530

    removed non-graphicals

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -377,75 +377,6 @@ function Dashboard() {
                                                 </div>
                                             </div>
                                         </div>
-
-                                        {/* <!-- Color System --> */}
-                                        <div className="row">
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-primary text-white shadow">
-                                                    <div className="card-body">
-                                                        Primary
-                                                        <div className="text-white-50 small">#4e73df</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-success text-white shadow">
-                                                    <div className="card-body">
-                                                        Success
-                                                        <div className="text-white-50 small">#1cc88a</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-info text-white shadow">
-                                                    <div className="card-body">
-                                                        Info
-                                                        <div className="text-white-50 small">#36b9cc</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-warning text-white shadow">
-                                                    <div className="card-body">
-                                                        Warning
-                                                        <div className="text-white-50 small">#f6c23e</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-danger text-white shadow">
-                                                    <div className="card-body">
-                                                        Danger
-                                                        <div className="text-white-50 small">#e74a3b</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-secondary text-white shadow">
-                                                    <div className="card-body">
-                                                        Secondary
-                                                        <div className="text-white-50 small">#858796</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-light text-black shadow">
-                                                    <div className="card-body">
-                                                        Light
-                                                        <div className="text-black-50 small">#f8f9fc</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 mb-4">
-                                                <div className="card bg-dark text-white shadow">
-                                                    <div className="card-body">
-                                                        Dark
-                                                        <div className="text-white-50 small">#5a5c69</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-
                                     </div>
 
                                     <div className="col-lg-6 mb-4">
@@ -468,21 +399,7 @@ function Dashboard() {
                                                     unDraw &rarr;</a>
                                             </div>
                                         </div>
-
                                         {/* <!-- Approach --> */}
-                                        <div className="card shadow mb-4">
-                                            <div className="card-header py-3">
-                                                <h6 className="m-0 font-weight-bold text-primary">Development Approach</h6>
-                                            </div>
-                                            <div className="card-body">
-                                                <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classNamees in order to reduce
-                                                    CSS bloat and poor page performance. Custom CSS classNamees are used to create
-                                                    custom components and custom utility classNamees.</p>
-                                                <p className="mb-0">Before working with this theme, you should become familiar with the
-                                                    Bootstrap framework, especially the utility classNamees.</p>
-                                            </div>
-                                        </div>
-
                                     </div>
                                 </div>
 
@@ -496,7 +413,7 @@ function Dashboard() {
                         <footer className="sticky-footer bg-white">
                             <div className="container my-auto">
                                 <div className="copyright text-center my-auto">
-                                    <span>Copyright &copy; Your Website 2021</span>
+                                    <span>Copyright &copy; 2023</span>
                                 </div>
                             </div>
                         </footer>
@@ -537,5 +454,4 @@ function Dashboard() {
         </div>
     )
 }
-
 export default Dashboard;
